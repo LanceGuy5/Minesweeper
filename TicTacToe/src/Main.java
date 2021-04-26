f76e09ce0942af427fafbffedc30849bed15afb3
LanceGuy5 <46636036+LanceGuy5@users.noreply.github.com>
2021-04-26 08:24:13 -0400

    Delete TicTacToe directory

--- TicTacToe/src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Lance Hartman
- * Written entirely by Lance Hartman for AP Computer Science project
- */
-public class Main {
-    public static void main(String[] args){
-        new TicTacToe();
-    }
-}
